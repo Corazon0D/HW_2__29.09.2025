9b1b36399fc44b2603fb6f2e27ff64cd2e3234d3
sanda <korazon567@gmail.com>
2025-09-29 18:54:07 +0300

    Palindrome

---
+++ H_W_29.09.2025.py
@@ -0,0 +1,15 @@
+import random
+
+string = input('Введите данные: ')
+x1 = string.upper() # Все буквы большие
+x2 = string.lower() # Все буквы маленькие
+
+# Вероятность того или другого варианта
+pu_pu_pu = [x1, x2]
+
+x3 = random.choice(pu_pu_pu)
+
+if str(string) == str(string)[::-1]:
+    print(x3, '- это палиндром')
+else:
+    print(x3, '- это не палиндром')
